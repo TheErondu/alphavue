4c6c262b3ac9897a3fa7e481048df0ed7c2469fa
TheErondu <erone007@gmail.com>
2023-10-10 12:02:57 +0100

    new updates

---
+++ resources/views/layouts/breadcrumbs.blade.php
@@ -0,0 +1,17 @@
+               <!-- breadcrumb start -->
+               <section class="breadcrumb-section p-0">
+                <img src="assets/images/inner-background.jpg" class="bg-img img-fluid" alt="title image">
+                <div class="container">
+                   <div class="breadcrumb-content">
+                      <div>
+                         <h1 class="text-white">Our Projects</h1>
+                         <nav aria-label="breadcrumb" class="theme-breadcrumb">
+                            <ol class="breadcrumb">
+                               <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                               <li class="breadcrumb-item active" aria-current="page">Projects</li>
+                            </ol>
+                         </nav>
+                      </div>
+                   </div>
+                </div>
+             </section>
